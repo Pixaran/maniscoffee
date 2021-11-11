1ceaa6b20592df2fa7e308f00abc8e4f37731696
Pixaran <pixsaran7@gmail.com>
2021-11-11 17:25:51 +0530

    fix

--- beta/images/js/custom.js
+++ beta/images/js/custom.js
@@ -4,7 +4,7 @@
 
     // PRE LOADER
     $(window).load(function(){
-      $('.spinner-wrapper').fadeOut(1000); // set duration in brackets    
+      $('.preloader').fadeOut(1000); // set duration in brackets    
     });
 
 
@@ -31,20 +31,7 @@
       autoplay: true,
       smartSpeed: 1000,
     })
-    
-	/* Rotating Text - Morphtext */
-	$("#js-rotating").Morphext({
-		// The [in] animation type. Refer to Animate.css for a list of available animations.
-		animation: "fadeIn",
-		// An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
-		separator: ",",
-		// The delay between the changing of each phrase in milliseconds.
-		speed: 2000,
-		complete: function () {
-			// Called after the entrance animation is executed.
-		}
-    });
-    
+
 
     // PARALLAX EFFECT
     $.stellar({
