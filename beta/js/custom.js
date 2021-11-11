2db605660c5199a2d05e2c565c62598572c1afed
Pixaran <pixsaran7@gmail.com>
2021-11-11 19:44:52 +0530

    fix

--- beta/js/custom.js
+++ beta/js/custom.js
@@ -31,20 +31,7 @@
       autoplay: true,
       smartSpeed: 1000,
     })
-    
-	/* Rotating Text - Morphtext */
-	$("#js-rotating").Morphext({
-		// The [in] animation type. Refer to Animate.css for a list of available animations.
-		animation: "fadeIn",
-		// An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
-		separator: ",",
-		// The delay between the changing of each phrase in milliseconds.
-		speed: 2000,
-		complete: function () {
-			// Called after the entrance animation is executed.
-		}
-    });
-    
+
 
     // PARALLAX EFFECT
     $.stellar({
@@ -110,7 +97,17 @@
     });
 
 */
-
+	$("#js-rotating").Morphext({
+		// The [in] animation type. Refer to Animate.css for a list of available animations.
+		animation: "fadeIn",
+		// An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
+		separator: ",",
+		// The delay between the changing of each phrase in milliseconds.
+		speed: 2000,
+		complete: function () {
+			// Called after the entrance animation is executed.
+		}
+    });
 
     // SMOOTHSCROLL
     $(function() {
@@ -122,7 +119,19 @@
             event.preventDefault();
       });
     });  
-
+	
+    /* Rotating Text - Morphtext */
+	$("#js-rotating").Morphext({
+		// The [in] animation type. Refer to Animate.css for a list of available animations.
+		animation: "fadeIn",
+		// An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
+		separator: ",",
+		// The delay between the changing of each phrase in milliseconds.
+		speed: 2000,
+		complete: function () {
+			// Called after the entrance animation is executed.
+		}
+    });
 
     // WOW ANIMATION
     new WOW({ mobile: false }).init();
